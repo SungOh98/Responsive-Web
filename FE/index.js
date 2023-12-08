1bc733ab8429a3242200fe3562eef0dd23cd8784
Jung, SungOh <tryjso@gmail.com>
2023-12-09 00:11:05 +0900

    FE/moblieVer

---
+++ FE/index.js
@@ -0,0 +1,6 @@
+function onMenuClick() {
+    var navbar = document.getElementById('nav-menu');
+    var responsive_class_name = 'responsive'
+  
+    navbar.classList.toggle(responsive_class_name)
+}
